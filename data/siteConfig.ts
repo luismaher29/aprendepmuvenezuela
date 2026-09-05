d6ec6fa8c524e9a02375d072e6da4536be3540de
luismaher29 <125908170+luismaher29@users.noreply.github.com>
2026-09-05 12:54:05 -0500

    Add files via upload

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,7 +1,13 @@
 export const siteConfig = {
   whatsappNumber: '573156833690',
   whatsappMessage: 'Hola, estoy en Venezuela 🇻🇪 y quiero información sobre los cursos de Aprende PMU.',
-  instagramUrl: 'https://instagram.com/', canonicalUrl: 'https://www.aprendemicroblading.com/venezuela',
+  canonicalUrl: 'https://aprendepmuvenezuela.vercel.app',
+  socialLinks: [
+    { name: 'Instagram', url: 'https://www.instagram.com/veronicapmu/', mark: 'IG' },
+    { name: 'TikTok', url: 'https://www.tiktok.com/@veronicapmu', mark: 'TT' },
+    { name: 'YouTube', url: 'https://www.youtube.com/@veronicampmu', mark: 'YT' },
+    { name: 'Facebook', url: 'https://www.facebook.com/veronicampmu/', mark: 'FB' },
+  ],
   trustLine: ['Formación internacional','Miles de estudiantes','Aprende a tu ritmo'],
   metrics: [{value:'+17',label:'Años de experiencia'},{value:'+20.000',label:'Estudiantes formados'},{value:'48',label:'Países alcanzados'},{value:'+15',label:'Productos disponibles'}],
 };
